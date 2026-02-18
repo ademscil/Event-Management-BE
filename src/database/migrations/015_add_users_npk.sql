585e38054ff949139942ff8e9abed9210d29a391
ademscil <adamjuliansyahcv@gmail.com>
2026-02-19 06:55:00 +0700

    fix(migration): make users npk migration sql-server safe

--- src/database/migrations/015_add_users_npk.sql
+++ src/database/migrations/015_add_users_npk.sql
@@ -15,14 +15,22 @@ BEGIN
     CREATE INDEX IX_Users_NPK ON Users(NPK);
 END;
 
+DECLARE @sql NVARCHAR(MAX);
+
 -- Keep existing numeric usernames as NPK if not set yet
+SET @sql = N'
 UPDATE Users
 SET NPK = Username
-WHERE (NPK IS NULL OR LTRIM(RTRIM(NPK)) = '')
-  AND Username NOT LIKE '%[^0-9]%';
+WHERE (NPK IS NULL OR LTRIM(RTRIM(NPK)) = '''')
+  AND Username NOT LIKE ''%[^0-9]%'';
+';
+EXEC sp_executesql @sql;
 
 -- Explicitly set superadmin NPK
+SET @sql = N'
 UPDATE Users
-SET NPK = '0676'
-WHERE Username = 'superadmin'
-  AND (NPK IS NULL OR LTRIM(RTRIM(NPK)) = '');
+SET NPK = ''0676''
+WHERE Username = ''superadmin''
+  AND (NPK IS NULL OR LTRIM(RTRIM(NPK)) = '''');
+';
+EXEC sp_executesql @sql;
